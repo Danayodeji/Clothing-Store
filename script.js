ddb7510fdae5a2917ff40fa16e12aef9fba9704c
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2025-08-01 16:52:35 +0100

    Complete the entire landing page responsiveness across all screen sizes

--- script.js
+++ script.js
@@ -13,3 +13,13 @@
     prevEl: ".swiper-button-prev",
   },
 });
+
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = "none";
+}
